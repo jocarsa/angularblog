79f39092c0f36e6dc1e76a311a08231924e0b250
jocarsa <info@josevicentecarratala.com>
2024-02-15 12:31:20 +0100

    pancartas en mongo

---
+++ Servidor/001-Mongo/005-insertar pancartas.js
@@ -0,0 +1,16 @@
+db.pancarta.insertMany(
+    [
+        {
+            'titulo':'Programación',
+            'subtitulo':'Programo de todo'
+        },
+        {
+            'titulo':'Formación',
+            'contenido':'Presencial y online'
+        },
+        {
+            'titulo':'Diseño',
+            'contenido':'Diseño 3d'
+        }
+    ]
+);
