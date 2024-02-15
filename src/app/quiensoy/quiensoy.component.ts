2704370f837504ddceb745c0b0497525e36b270a
jocarsa <info@josevicentecarratala.com>
2024-02-15 12:52:49 +0100

    obtener json

--- src/app/quiensoy/quiensoy.component.ts
+++ src/app/quiensoy/quiensoy.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { SobremiService } from '../sobremi.service';
 @Component({
   selector: 'app-quiensoy',
   templateUrl: './quiensoy.component.html',
   styleUrl: './quiensoy.component.css'
 })
 export class QuiensoyComponent {
+jsonData: any;
 
+  constructor(private sobremiService: SobremiService) { }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData() {
+    this.sobremiService.getData().subscribe(data => {
+      this.jsonData = data;
+      console.log(data);
+    });
+  }
 }
