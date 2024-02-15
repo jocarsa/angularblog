8673b9db2f1ea100ebbbccd58d5383ff4308d536
jocarsa <info@josevicentecarratala.com>
2024-02-15 12:49:03 +0100

    Sobre mi y pancarta

---
+++ Servidor/001-Mongo/007-insertar sobre mi.js
@@ -0,0 +1,20 @@
+db.sobremi.insertMany(
+    [
+        {
+            'titulo':'Titulo',
+            'contenido':'Diseñador Industrial. Experto en programación y gráficos 3d. Profesor en formación tanto presencial como online. Profesor en LinkedIn Learning y en Mastermedia. CEO y Fundador en JOCARSA. Desarrollo de Proyectos de Robótica, Infografía CGI, Visualización 3D Interactiva en Realidad Virtual operando el estado del arte de la tecnología.'
+        },
+        {
+            'titulo':'Titulo',
+            'contenido':'Diseñador Industrial. Experto en programación y gráficos 3d. Profesor en formación tanto presencial como online. Profesor en LinkedIn Learning y en Mastermedia. CEO y Fundador en JOCARSA. Desarrollo de Proyectos de Robótica, Infografía CGI, Visualización 3D Interactiva en Realidad Virtual operando el estado del arte de la tecnología.'
+        },
+        {
+            'titulo':'Titulo',
+            'contenido':'Diseñador Industrial. Experto en programación y gráficos 3d. Profesor en formación tanto presencial como online. Profesor en LinkedIn Learning y en Mastermedia. CEO y Fundador en JOCARSA. Desarrollo de Proyectos de Robótica, Infografía CGI, Visualización 3D Interactiva en Realidad Virtual operando el estado del arte de la tecnología.'
+        },
+        {
+            'titulo':'Titulo',
+            'contenido':'Diseñador Industrial. Experto en programación y gráficos 3d. Profesor en formación tanto presencial como online. Profesor en LinkedIn Learning y en Mastermedia. CEO y Fundador en JOCARSA. Desarrollo de Proyectos de Robótica, Infografía CGI, Visualización 3D Interactiva en Realidad Virtual operando el estado del arte de la tecnología.'
+        }
+    ]
+);
