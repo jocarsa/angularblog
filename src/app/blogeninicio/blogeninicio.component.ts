935d1fa5d07d41262ebc656b762065daaf4565c9
jocarsa <info@josevicentecarratala.com>
2024-02-15 13:16:13 +0100

    componente de nuevo

---
+++ src/app/blogeninicio/blogeninicio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blogeninicio',
+  templateUrl: './blogeninicio.component.html',
+  styleUrl: './blogeninicio.component.css'
+})
+export class BlogeninicioComponent {
+
+}
