4b13ca3270df98e86dd3c1a62f5b03450e08bad1
jocarsa <info@josevicentecarratala.com>
2024-02-15 10:49:08 +0100

    creación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { BlogComponent } from './blog/blog.component';
+import { QuiensoyComponent } from './quiensoy/quiensoy.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    BlogComponent,
+    QuiensoyComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quiensoy/quiensoy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiensoy',
+  templateUrl: './quiensoy.component.html',
+  styleUrl: './quiensoy.component.css'
+})
+export class QuiensoyComponent {
+
+}
